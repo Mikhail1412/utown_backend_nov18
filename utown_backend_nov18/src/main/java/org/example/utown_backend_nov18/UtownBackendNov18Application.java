4405602d5f93c9c140d6f2060b3f68c0dc9d77b6
misa <mischa.zeu1998@gmail.com>
2025-11-25 17:01:31 +0900

    Initialize Spring Boot 4.0.0 project with Lombok

---
+++ utown_backend_nov18/src/main/java/org/example/utown_backend_nov18/UtownBackendNov18Application.java
@@ -0,0 +1,13 @@
+package org.example.utown_backend_nov18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtownBackendNov18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtownBackendNov18Application.class, args);
+	}
+
+}
---
+++ utown_backend_nov18/src/test/java/org/example/utown_backend_nov18/UtownBackendNov18ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.utown_backend_nov18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtownBackendNov18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
